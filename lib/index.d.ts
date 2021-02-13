3bf01eb081b13030e089545764dc32da6c2a0c66
ZZhanakhmetova <zzhanakhmetova@beeline.kz>
2021-02-13 16:17:47 +0600

    initial

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+declare const _default: {
+    Button: import("react").FC<import("./stories/Button").ButtonProps>;
+    Header: import("react").FC<import("./stories/Header").HeaderProps>;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
